a76600d8e0d48be2a5f74e4d56eaf3ebedb2a07a
Aronsoyol <aronsoyol@gmail.com>
2018-11-04 14:18:15 +0900

    imrove setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,20 @@
 
 # from distutils.core import setup
 from setuptools import setup
-      install_requires=['freetype-py',
-                        'numpy',
-                        'pillow',
-                        'imagehash',
-                        'pygobject'],
-      packages=['timu'],
-      include_package_data=True,
-      package_data={'timu': ['MongolianWhite3.ttf']},
-      )
+setup(
+    name='timu',
+    version='1.0.2',
+    description='Traditional Mongolian Word Unifer',
+    author='Aronsoyol',
+    author_email='aronsoyol@gmail.com',
+    url='https://github.com/aronsoyol/timu',
+    install_requires=[
+        'freetype-py',
+        'numpy',
+        'pillow',
+        'imagehash',
+        'pygobject'],
+    packages=['timu'],
+    include_package_data=True,
+    package_data={'timu': ['MongolianWhite3.ttf']},
+)
