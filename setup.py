c52c8d8e0ddaa6d1156fcfc0f3dd6dff817f8202
Aronsoyol <aronsoyol@gmail.com>
2018-11-04 14:17:22 +0900

    distutils -> setuptools

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 
 # coding: utf-8
 
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(name='timu',
-      version='1.0.2',
-      description='Traditional Mongolian Word Unifer',
-      author='Aronsoyol',
-      author_email='aronsoyol@gmail.com',
-      url='https://github.com/aronsoyol/timu',
+# from distutils.core import setup
+from setuptools import setup
       install_requires=['freetype-py',
                         'numpy',
                         'pillow',
