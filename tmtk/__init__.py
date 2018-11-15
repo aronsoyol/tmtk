fc6d1e4a3caa53d9fc9e4446dbfe08fb11f69c9f
Aronsoyol <aronsoyol@gmail.com>
2018-11-15 16:41:52 +0900

    init module

--- tmtk/__init__.py
+++ tmtk/__init__.py
@@ -1,13 +1,32 @@
 
 # from .shaper import Shaper
 
-# from .shaper2 import shaper
+from .__shaper2 import Shaper2
 
-# from .unifier import Unifier
+from .__converter import convert2unicode
+from .__unifier import Unifier
 
-# from .token import Tokenizer
+from .__token import Tokenizer
 
 # from . convert2unicode import Converter
 
-# unifier = Unifier()
-# tokenizer = Tokenizer()
+unifier = Unifier()
+tokenizer = Tokenizer()
+shaper = Shaper2()
+
+
+class Uniqode():
+    __word = ""
+
+    def __init__(self, word):
+        self.__word = word
+        self.__garray = unifier.get_garray(word)
+
+    def __hash__(self):
+        return hash(str(self.__garray))
+
+    def __eq__(self, other):
+        return self.__garray == other.__garray
+
+    def __str__(self):
+        return str(self.__garray)
